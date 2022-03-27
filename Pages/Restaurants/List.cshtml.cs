ec06d2b710d21baa220188ec53432e8e1f911b68
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-27 20:14:15 +0530

    register data service

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
         private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
 
         public string Message { get; set; }
 
-        public ListModel(IConfiguration config)
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
+            this.restaurantData = restaurantData;
         }
         public void OnGet()
         {
